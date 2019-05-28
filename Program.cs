11754a53724604baff8a7773695d706199560e47
Rose Wisotzky <rose@rosewisotzky.com>
2019-05-28 15:52:02 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace car_lot_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
